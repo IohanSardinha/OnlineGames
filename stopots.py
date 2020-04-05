34b5041b8ab7e0d1ae22fd0db1102444ae0fb3e0
Iohan Sardinha <ioxsardinha@gmail.com>
2020-04-05 22:40:09 +0100

    A bit better this way

--- stopots.py
+++ stopots.py
@@ -5,6 +5,34 @@ from os import path
 import unicodedata
 
 data = {}
+driver = ''
+
+def main():
+    if not path.exists('stopots.json'):
+        f = open('stopots.json','w')
+        f.write('{}')
+        f.close()
+    
+    file = open("stopots.json",'r')
+    global data
+    data = json.loads(file.read())
+    file.close()
+    i = input("Abrir navegador - 0\nJogar - 1\nPovoar - 2\nVer - 3\nCopiar categoria - 4\nSair - 5\n>")
+    if(i == '0'):
+        browser()
+    elif(i == '1'):
+        play()
+    elif(i == '2'):
+        insert()
+    elif(i == '3'):
+        show()
+    elif(i == '4'):
+        copy()
+    elif not driver == '':
+        driver.quit()
+        return
+    else:
+        return
 
 def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
@@ -22,17 +50,29 @@ def addCategory(c):
         for i in range(ord('a'),ord('z')+1):
             data[c][chr(i)] = []
 
-def play():
+def browser(ret = main):
+    global driver
     url = input('URL\n>')
     if 'https://' not in url:
         url = 'https://' + url
 
     driver = webdriver.Opera()
     driver.get(url)
+    ret()
+
+def play():
+    if driver == '':
+        browser(play)
+
     while True:
-        l = input('Letra (0 sair)').lower()
+        l = input('Letra\n0 - sair\n1 - avaliar/estou pronto)\n>').lower()
+        
         if l == '0':
             break
+        if l == '1':
+            btn = driver.find_element_by_xpath('//*[@id="screenGame"]/div[2]/div[2]/div/button/strong')
+            btn.click()
+
         for i in range(1,13):
             tema = driver.find_element_by_xpath('//*[@id="screenGame"]/div[2]/div[2]/div/div[1]/label[{}]/span'.format(i)).text        
             tema = strip_accents(tema).lower()
@@ -41,7 +81,6 @@ def play():
             if (tema in data.keys() and len(data[tema][l]) > 0):
                 inp.send_keys(data[tema][l][ri(0,len(data[tema][l])-1)])          
     save()
-    driver.quit()
     main()
     
 def insert():
@@ -55,11 +94,11 @@ def insert():
                 data[c] = {}
                 for i in range(ord('a'),ord('z')+1):
                     data[c][chr(i)] = []
-            l = input("\tLetra (0 sair)\n>").lower()
+            l = input("\tLetra (0 sair)\n\t>").lower()
             if l == '0':
                 break
             while True:
-                p = input('\t\tPalavra (0 sair)\n>').lower()
+                p = input('\t\tPalavra (0 sair)\n\t\t>').lower()
                 if p == '0':
                     break
                 data[c][l].append(p)
@@ -77,7 +116,7 @@ def show():
             print(data[c[0:-3]])
             continue
         while True:
-            l = input('Letra (0 sair)\n>').lower()
+            l = input('\tLetra (0 sair)\n\t>').lower()
             if l == '0':
                 break
             print(data[c][l])
@@ -91,27 +130,8 @@ def copy():
             break
         a = input("Antiga\n>").lower()
         data[n] = data[a]
+    save()
     show()
-
-def main():
-    if not path.exists('stopots.json'):
-        f = open('stopots.json','w')
-        f.write('{}')
-        f.close()
-    
-    file = open("stopots.json",'r')
-    global data
-    data = json.loads(file.read())
-    file.close()
-    i = input("Jogar - 0\nPovoar - 1\nVer - 2\nCopiar categoria - 3\nSair - 4\n>")
-    if(i == '0'):
-        play()
-    if(i == '1'):
-        insert()
-    if(i == '2'):
-        show()
-    if(i == '3'):
-        copy()
 
 if __name__ == '__main__':
     main()
